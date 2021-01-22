27c35fe49841d9db611ed644d52198837498d533
zeushere <kacper.roda37@gmail.com>
2021-01-22 22:44:48 +0100

    Searching added

--- src/main/java/config/NaprawaController.java
+++ src/main/java/config/NaprawaController.java
@@ -2,12 +2,16 @@ package config;
 
 import connect.DbConnect;
 import database.Naprawa;
+import database.Usluga;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -29,6 +33,8 @@ public class NaprawaController implements Initializable {
     private TableColumn<Naprawa, String> naprawaWlasciciel;
     @FXML
     private TableColumn<Naprawa, String> naprawaSamochod;
+    @FXML
+    private TextField filterField;
 
     private Connection connection;
     private DbConnect dbConnect;
@@ -47,7 +53,12 @@ public class NaprawaController implements Initializable {
     private void naprawa() {
         try {
             naprawy = FXCollections.observableArrayList();
-            String select = "SELECT *, CONCAT(usluga.nazwa_uslugi) as un, naprawa.data_naprawy, CONCAT(naprawa.koszt_naprawy, ' zł') as kosztPLN, CONCAT(wlasciciel.imie_wlasciciela,' ',wlasciciel.nazwisko_wlasciciela) as wimnw, CONCAT(wlasciciel.marka_samochodu_wlasciciela,' ',wlasciciel.model_samochodu_wlasciciela) as wmamo FROM naprawa INNER JOIN usluga ON naprawa.id_uslugi=usluga.id_uslugi INNER JOIN wlasciciel ON naprawa.id_wlasciciela=wlasciciel.id_wlasciciela";
+            String select = "SELECT *, CONCAT(usluga.nazwa_uslugi) as un, " +
+                    "naprawa.data_naprawy, CONCAT(naprawa.koszt_naprawy, ' zł') as kosztPLN, " +
+                    "CONCAT(wlasciciel.imie_wlasciciela,' ',wlasciciel.nazwisko_wlasciciela) as wimnw, " +
+                    "CONCAT(wlasciciel.marka_samochodu_wlasciciela,' ',wlasciciel.model_samochodu_wlasciciela) as wmamo " +
+                    "FROM naprawa INNER JOIN usluga ON naprawa.id_uslugi=usluga.id_uslugi " +
+                    "INNER JOIN wlasciciel ON naprawa.id_wlasciciela=wlasciciel.id_wlasciciela";
             connection = dbConnect.getConnection();
             ResultSet resultSet = connection.createStatement().executeQuery(select);
             while (resultSet.next()){
@@ -69,7 +80,57 @@ public class NaprawaController implements Initializable {
             naprawaWlasciciel.setCellValueFactory(new PropertyValueFactory<>("wimnw"));
             naprawaSamochod.setCellValueFactory(new PropertyValueFactory<>("wmamo"));
 
-            naprawaTable.setItems(naprawy);
+            FilteredList<Naprawa> filteredData = new FilteredList<>(naprawy, b-> true);
+
+            filterField.textProperty().addListener(((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(naprawa->{
+                    if(newValue == null || newValue.isEmpty())
+                    {
+                        return true;
+                    }
+
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if(naprawa.getWmamo().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+                    else if(naprawa.getWimnw().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+                    else if(naprawa.getUn().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+                    else if(naprawa.getData_naprawy().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+                    else if(naprawa.getWimnw().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+                    else if(naprawa.getKosztPLN().toLowerCase().indexOf(lowerCaseFilter) != -1)
+                    {
+                        return true;
+                    }
+
+                    else {
+                        return false;
+                    }
+                });
+
+
+            }));
+
+            SortedList<Naprawa> sortedData = new SortedList<>(filteredData);
+
+            sortedData.comparatorProperty().bind(naprawaTable.comparatorProperty());
+
+            naprawaTable.setItems(sortedData);
+
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
