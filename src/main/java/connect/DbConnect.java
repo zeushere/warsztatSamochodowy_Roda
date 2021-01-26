4dff4cc81c7c9729221b910425a3897062332a90
zeushere <kacper.roda37@gmail.com>
2021-01-26 14:24:24 +0100

    Fixed db connect

--- src/main/java/connect/DbConnect.java
+++ src/main/java/connect/DbConnect.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnect {
     protected Connection connection;
     public Connection getConnection(){
-        final String connectionString="jdbc:mysql://localhost:3306/warsztat_samochodowy2";
+        final String connectionString="jdbc:mysql://localhost:3306/warsztat_samochodowy";
         try {
             connection = DriverManager.getConnection(connectionString,"root", "");
             return connection;
